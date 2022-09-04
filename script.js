3eb847dddca004bb04265787af0fb51ecb861e10
derkaiserr <oluadebojnr@gmail.com>
2022-09-04 06:24:48 +0100

    update

---
+++ script.js
@@ -0,0 +1,44 @@
+let lines = document.querySelector('.lines')
+let phoneNav = document.querySelector('.draw-out')
+let phoneNavv = document.getElementById('draw-out')
+let webNav = document.querySelector('.nav')
+let x = document.querySelector('.x')
+let body = document.querySelector('body')
+let section = document.querySelector('.section-one')
+let sectionT = document.querySelector('.section-two')
+let header = document.querySelector('header')
+
+phoneNav.style.display = 'none'
+lines.addEventListener('click', function(){
+    // phoneNavv.style.transition = 'all 6s ease-in'
+    phoneNav.style.display = 'block'
+    section.style.transition = 'all 0.4s ease-in'
+    body.style.transition = 'all 0.4s ease-in'
+    header.style.transition = 'all 0.4s ease-in'
+    phoneNav.style.visibility = 'visible'
+    body.style.overflow = 'hidden'
+    section.style.filter = 'blur(0.2rem)'
+    sectionT.style.filter = 'blur(0.2rem)'
+    body.style.backgroundColor = 'grey'
+    sectionT.style.backgroundColor = 'grey'
+    header.style.backgroundColor = 'grey'
+    header.style.filter = 'blur(0.2rem)'
+    phoneNav.style.transform = 'translateX(-200px)'
+    // phoneNavv.style.transform += 'translateX(70px)'
+    
+}, false)
+
+x.addEventListener('click', function(){
+    header.style.filter = 'blur(0rem)'
+    phoneNav.style.transform = 'translateX(200px)'
+    section.style.filter = 'blur(0rem)'
+    phoneNav.style.visibility = 'hidden'
+    sectionT.style.filter = 'blur(0rem)'
+    body.style.overflow = 'visible'
+    sectionT.style.backgroundColor = '#f0efef'
+    body.style.backgroundColor = 'white'
+    header.style.backgroundColor = '#f0efef'
+    
+} )
+
+
